55670cd228fe6061a9e0162327a5f12263b70f31
ellie-fisher <ellie.am.fisher@gmail.com>
2026-01-14 17:39:15 -0800

    Initial decompilation of `aSHOP_actor_move()`

--- src/overlays/actors/ovl_Shop/ac_shop.c
+++ src/overlays/actors/ovl_Shop/ac_shop.c
@@ -1,6 +1,8 @@
 #include "ac_shop.h"
 #include "m_actor_dlftbls.h"
 #include "m_object.h"
+#include "macros.h"
+#include "code_variables.h"
 #include "overlays/gamestates/ovl_play/m_play.h"
 
 #define THIS ((Shop*)thisx)
@@ -10,6 +12,7 @@ void aSHOP_actor_dt(Actor* thisx, Game_Play* game_play);
 void aSHOP_actor_init(Actor* thisx, Game_Play* game_play);
 void aSHOP_actor_draw(Actor* thisx, Game_Play* game_play);
 void aSHOP_actor_move(Actor* thisx, Game_Play* game_play);
+void func_80A0E1F0_jp(Actor* thisx);
 
 #if 0
 ActorProfile Shop_Profile = {
@@ -53,10 +56,22 @@ ActorProfile Shop_Profile = {
 
 #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Shop/ac_shop/func_80A0E4A8_jp.s")
 
-#pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Shop/ac_shop/aSHOP_actor_move.s")
+// #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Shop/ac_shop/aSHOP_actor_move.s")
+void aSHOP_actor_move(Actor* thisx, Game_Play* game_play) {
+    Shop* this = THIS;
+    StructureActor* shop = &this->structureActor;
+
+    SegmentBaseAddress[6] =
+        (uintptr_t)OS_PHYSICAL_TO_K0(common_data.clip.structureClip->getObjectSegment(STRUCTURE_TYPE_SHOP));
+    shop->unk_174 = cKF_SkeletonInfo_R_play(&shop->skeletonInfo);
+    shop->unk_1E8 = shop->skeletonInfo.frameControl.currentFrame;
+
+    ((ShopActionFunc)shop->process)(this, game_play);
+    func_80A0E1F0_jp(thisx);
+}
 
 // #pragma GLOBAL_ASM("asm/jp/nonmatchings/overlays/actors/ovl_Shop/ac_shop/aSHOP_actor_init.s")
-void aSHOP_actor_init(Actor *thisx, Game_Play *game_play) {
+void aSHOP_actor_init(Actor* thisx, Game_Play* game_play) {
     Shop* this = THIS;
     StructureActor* shop = &this->structureActor;
 
